65d68136990cc7ed04a51976d3bb98fed9ae791a
pilo <jakub.pilimon@4finance.com>
2016-10-06 17:34:40 +0200

    corrected e2e

--- src/test/groovy/io/pillopl/eventsource/shop/integration/E2ESpec.groovy
+++ src/test/groovy/io/pillopl/eventsource/shop/integration/E2ESpec.groovy
@@ -3,6 +3,7 @@ package io.pillopl.eventsource.shop.integration
 import io.pillopl.eventsource.shop.domain.events.ItemOrdered
 import io.pillopl.eventsource.shop.domain.events.ItemPaid
 import io.pillopl.eventsource.shop.domain.events.ItemPaymentTimeout
+import io.pillopl.eventsource.shop.eventstore.publisher.EventPublisher
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.stream.messaging.Sink
 import org.springframework.cloud.stream.messaging.Source
@@ -13,13 +14,16 @@ import spock.util.concurrent.PollingConditions
 
 import java.util.concurrent.BlockingQueue
 
+import static java.util.UUID.randomUUID
+
 class E2ESpec extends IntegrationSpec {
 
-    private UUID anyUuid = UUID.randomUUID()
+    private ANY_UUID = randomUUID()
 
     @Autowired Source source
     @Autowired Sink commands
     @Autowired MessageCollector eventsCollector
+    @Autowired EventPublisher eventPublisher
 
     BlockingQueue<Message<?>> events
     PollingConditions conditions = new PollingConditions(timeout: 12, initialDelay: 0, factor: 1)
@@ -30,8 +34,9 @@ class E2ESpec extends IntegrationSpec {
 
     def 'received order command should result in emitted item ordered event'() {
         when:
-            commands.input().send(new GenericMessage<>(sampleOrderInJson(anyUuid)))
-       then:
+            commands.input().send(new GenericMessage<>(sampleOrderInJson(ANY_UUID)))
+
+        then:
             conditions.eventually {
                 expectedMessageThatContains(ItemOrdered.TYPE)
             }
@@ -39,9 +44,10 @@ class E2ESpec extends IntegrationSpec {
 
     def 'received pay command should result in emitted item paid event'() {
         when:
-            commands.input().send(new GenericMessage<>(sampleOrderInJson(anyUuid)))
+            commands.input().send(new GenericMessage<>(sampleOrderInJson(ANY_UUID)))
         and:
-            commands.input().send(new GenericMessage<>(samplePayInJson(anyUuid)))
+            commands.input().send(new GenericMessage<>(samplePayInJson(ANY_UUID)))
+
         then:
             conditions.eventually {
                 expectedMessageThatContains(ItemOrdered.TYPE)
@@ -54,9 +60,10 @@ class E2ESpec extends IntegrationSpec {
 
     def 'received mark missing payment command should result in emitted marked as missed event'() {
         when:
-            commands.input().send(new GenericMessage<>(sampleOrderInJson(anyUuid)))
+            commands.input().send(new GenericMessage<>(sampleOrderInJson(ANY_UUID)))
         and:
-            commands.input().send(new GenericMessage<>(sampleMarkTimeoutInJson(anyUuid)))
+            commands.input().send(new GenericMessage<>(sampleMarkTimeoutInJson(ANY_UUID)))
+
         then:
             conditions.eventually {
                 expectedMessageThatContains(ItemOrdered.TYPE)
@@ -72,16 +79,18 @@ class E2ESpec extends IntegrationSpec {
     }
 
     private static String samplePayInJson(UUID uuid) {
-        return "{\"type\":\"item.pay\",\"uuid\":\"$uuid\",\"when\":\"2016-10-06T10:28:23.956Z\"}"
+        return "{\"type\":\"item.pay\",\"uuid\":\"$uuid\",\"when\":\"2016-10-06T10:29:23.956Z\"}"
     }
 
     private static String sampleMarkTimeoutInJson(UUID uuid) {
-        return "{\"type\":\"item.markPaymentTimeout\",\"uuid\":\"$uuid\",\"when\":\"2016-10-06T10:28:23.956Z\"}"
+        return "{\"type\":\"item.markPaymentTimeout\",\"uuid\":\"$uuid\",\"when\":\"2016-10-06T10:29:23.956Z\"}"
     }
 
     void expectedMessageThatContains(String text) {
         Message<String> msg = events.poll()
-        assert msg != null
+        println msg
+        assert msg != null && msg.getPayload().contains(text)
+        println "GOT IT: contains " + msg.getPayload().contains(text)
         assert msg.getPayload().contains(text)
     }
 
